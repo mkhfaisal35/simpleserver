2e5a70b4bc5f0a3c392a32e443146e44f701a08f
Faisal <faisal3.bd@gmail.com>
2022-11-08 06:50:56 +0300

    my server

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/v1/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
